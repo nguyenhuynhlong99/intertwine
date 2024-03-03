574477ba249c32b27a94d18d581e12667ce0d7fd
Long Nguyen <longnguyen@Longs-MacBook-Air.local>
2024-03-03 14:32:57 -0800

    Updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'https://intertwine-server.onrender.com',
         // target: 'http://localhost:6000',
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
