e818ea55073d9e60587e34d43111e335541169b6
Long Nguyen <longnguyen@Longs-MacBook-Air.local>
2024-03-03 18:41:08 -0800

    Updated backend config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -69,19 +69,19 @@ app.use(
   })
 );
 
-// app.use(function (req, res, next) {
-//   res.header(
-//     'Access-Control-Allow-Credentials',
-//     'https://intertwine.onrender.com'
-//   );
-//   res.header('Access-Control-Allow-Origin', req.headers.origin);
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header(
+    'Access-Control-Allow-Credentials',
+    'https://intertwine.onrender.com'
+  );
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  );
+  next();
+});
 
 // Routes
 app.use('/api/users', userRoutes);
