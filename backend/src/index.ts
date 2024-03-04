aecefc10076a339fe782c838f9bffdaa41bdcf49
Long Nguyen <longnguyen@Longs-MacBook-Air.local>
2024-03-04 10:50:56 -0800

    Updated backend config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -58,7 +58,7 @@ app.use(
     proxy: true,
     name: 'intertwine-user-session',
     cookie: {
-      path: '/',
+      // path: '/',
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
       secure: true,
