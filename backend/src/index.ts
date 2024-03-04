e2d147a80316276911693b324ae8edf19d9c1452
Long Nguyen <longnguyen@Longs-MacBook-Air.local>
2024-03-03 18:48:09 -0800

    Updated backend config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -70,10 +70,6 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  res.header(
-    'Access-Control-Allow-Credentials',
-    'https://intertwine.onrender.com'
-  );
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header(
